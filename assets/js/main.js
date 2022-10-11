9faa0d786c84a50f07552ed1b932af9d79fe6e14
pouriya-moien <111745220+pouriya-moien@users.noreply.github.com>
2022-10-11 04:26:01 -0700

    return-radom-objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,10 @@ function random(min,max){
  	return Math.round(Math.random() * (max-min) + min);
 }
 
+function setBG(){
+    if (Math.round(Math.random())){
+      return "";
+    } else {
+      return "";
+    }
+  }
